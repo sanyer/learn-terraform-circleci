969f97b509c0533e9cf480d48814567443010f72
Roman Zhuzha <romazhuzha@gmail.com>
2021-10-22 21:12:57 +0200

    oopsie

--- main.tf
+++ main.tf
@@ -6,13 +6,14 @@ terraform {
     }
   }
   required_version = "> 0.14"
+
+  backend "s3" {
+    bucket = "circle-ci-backend-20211022190434119200000001"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
-backend "s3" {
-  bucket = "circle-ci-backend-20211022190434119200000001"
-  key    = "terraform/webapp/terraform.tfstate"
-  region = "us-east-1"
-}
 
 provider "aws" {
   region  = var.region
