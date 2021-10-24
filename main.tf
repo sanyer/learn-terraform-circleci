0c6e77f7224c5a7a6bf2ea3a22d35a58c0668381
Roman Zhuzha <romazhuzha@gmail.com>
2021-10-24 03:57:07 +0200

    explicitly specify required providers and their versions

--- main.tf
+++ main.tf
@@ -4,6 +4,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.44.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   required_version = "~> 1.0.0"
 
